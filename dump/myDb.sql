150fde20577e085ad7db8814342bcdd6d3855710
yoleo <yoleo@yandex.ru>
2024-12-24 00:00:02 +0200

    Init

---
+++ dump/myDb.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE IF NOT EXISTS myDb;
+USE myDb;
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+  time_zone = "+02:00";
+-- CREATE TABLE `Users` (
+--     `id` int(11) NOT NULL,
+--     `email` varchar(100) NOT NULL,
+--     `password` varchar(255) NOT NULL,
+--   ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- CREATE TABLE `Tours` (
+--     `id` int(11) NOT NULL,
+--     `email` varchar(100) NOT NULL,
+--     `password` varchar(255) NOT NULL,
+-- ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+-- INSERT INTO
+--   `Person` (`id`, `name`)
+-- VALUES
+--   (1, 'Conf'),
+--   (2, 'Nipsu'),
+--   (3, 'Beyar');
